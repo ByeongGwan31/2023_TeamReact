bcdd7c46afad0df50112bc861fef4f105267b7ef
bgwanii <2000bk@naver.com>
2023-06-07 20:28:55 +0900

    jsx 수정

--- src/Main.jsx
+++ src/Main.jsx
@@ -31,7 +31,7 @@ function App() {
         <p>누적 관객 수 : {movie.audiAcc}명</p>
         <p>영화 개봉일 : ({movie.openDt}) </p>
         <p>영화 코드 : ({movie.movieCd}) </p>
-        <button style={{ width:250, height:50, fontsize: 15}}onClick={() => console.log(`영화 코드 ${movie.movieCd}가 클릭되었습니다.`)}>영화 확인</button>
+        <button style={{ width:250, height:50, fontsize: 15}}onClick={() => console.log(`영화 코드 ${movie.movieCd}가 클릭 완료!`)}>영화 확인</button>
       </div>
     )}
   </div>
